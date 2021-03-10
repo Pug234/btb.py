111fd0196c76d334b652462a0ffcbd08822a343f
Pug234 <50160567+Pug234@users.noreply.github.com>
2021-03-09 19:54:01 -0700

    Update init

--- btbpug/__init__.py
+++ btbpug/__init__.py
@@ -4,7 +4,5 @@ class BytesToBits(object):
         from .lyrics import lyrics
         from .madlibs import MadLibs
         from .speedtext import SpeedText
-        from .reddit import reddit
-        from .meme import meme
     except Exception as e:
         raise e
