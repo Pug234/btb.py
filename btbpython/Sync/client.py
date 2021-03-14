a9c130bb62eddb36d5eb62910819605b9cef3015
Pug234 <50160567+Pug234@users.noreply.github.com>
2021-03-13 19:09:43 -0700

    test

--- btbpython/Sync/client.py
+++ btbpython/Sync/client.py
@@ -1,21 +1,30 @@
 import requests
 from typing import Union
-from .madlibs import madlibs
-from .lyrics import lyrics as lyric
-from .word import word as w
-from .text import Text as text
-from .reddit import reddit
-from .meme import meme
+
 
 class client:
   def __init__(self, token:str):
       self.token = {"Authorization": token}
 
+    def lyrics(token, song: str, artist: str = None):
+      song = song.replace(" ", "+")
+
+      song = f'https://api.bytestobits.dev/lyrics/?song={song}'
+
+      if artist != None:
+          artist = artist.replace(" ", "+")
+          song += f'&artist={artist}'
+
+      song = request(song, token)
+
+      if song == {"GeniusError": "Not Found"}:
+          raise TypeError("GeniusError: Song not found")
+
+      return song
+
   def word(self):
       return w(token=self.token)
 
-  def lyrics(self, song: str, artist: str = None):
-      return lyric(song=song, artist=artist, token=self.token)
 
   def MadLibs(self, random:bool=True, title:str=None, variables:Union[tuple, list]=None, text:str=None, questions:int=None):
       return madlibs(random=random, title=title, variables=variables, text=text, questions=questions, token = self.token)
@@ -28,3 +37,11 @@ class client:
 
   def Meme(self):
       return meme(token=self.token)
+
+def request(url, token):
+    r = requests.get(url, headers=token)
+
+    if r.status_code == 429:
+      raise Exception(f'API response: {r.status_code}: To many request')
+
+    return r.json()
--- test.py
+++ test.py
@@ -1,20 +1,4 @@
 import btbpython, asyncio
-client = btbpython.asyncClient("43ha.NfmK1rhf1EUyVhZ4MWPO")
+client = btbpython.client("43ha.NfmK1rhf1EUyVhZ4MWPO")
 
-async def test():
-    word = await client.word()
-    madlibs = await client.madlibs()
-    text = await client.text()
-    lyrics = await client.lyrics("Never too late")
-
-    lyrics2 = await client.lyrics("Never too late", "Three Days Grace")
-
-    with open("output.txt", "w+") as f:
-        f.write(f"{word}\n{madlibs}\n{text}\n{lyrics}\n\n{lyrics2}")
-
-loop = asyncio.get_event_loop()
-
-try:
-    loop.run_until_complete(test())
-finally:
-    loop.close()
+print(client.lyrics())
